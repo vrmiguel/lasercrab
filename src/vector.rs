e96bf8e0c6de844aa7bc131382c17ff052f89ba7
Vinícius Rodrigues Miguel <lemao.vrm07@hotmail.com>
2020-11-20 22:57:10 -0300

    WIP rotate_camera feature

--- src/vector.rs
+++ src/vector.rs
@@ -1,9 +1,7 @@
-// use nalgebra::{Vector3};
-
-// pub type Vec3f = Vector3<f64>;
-// pub type Vec2f = Vector2<f64>;
-
-
+//! This module contains super straightforward implementations
+//! for whatever we need from vectorial math.
+//! The entire implementation is simple, without any clever
+//! optimizations (although it did manage to beat Nalgebra in speed, somehow)
 
 use std::ops::Index;
 use std::ops::Mul;
@@ -156,4 +154,64 @@ impl Mul<f64> for Vec3f {
 #[allow(non_snake_case)]
 pub fn reflect(J: Vec3f, N: Vec3f) -> Vec3f {
     J - N * 2. * J.dot(&N)
+}
+
+#[cfg(feature = "rotate_camera")]
+pub fn rotate_camera(origin: Vec3f, dir: Vec3f, target: Vec3f)  -> Vec3f {
+   let z_axis  = (origin - target).normalize();
+   let x_axis  = Vec3f::new(0., 1., 0.).cross(&z_axis);
+   let y_axis  = z_axis.cross(&x_axis).normalize();
+
+   let transformation_matrix: Mat4f = [
+   		Vec4f::new( x_axis.x, x_axis.y, x_axis.z, 0. ),
+   		Vec4f::new( y_axis.x, y_axis.y, y_axis.z, 0. ),
+   		Vec4f::new( z_axis.x, z_axis.y, z_axis.z, 0. ),
+   		Vec4f::new( origin.x, origin.y, origin.z, 1. )
+   	];
+
+   let new_dir = transformation_matrix *
+   				 Vec4f::new(dir.x, dir.y, dir.z, 0.);
+
+   Vec3f::new(new_dir.x, new_dir.y, new_dir.z)
+}
+
+#[cfg(feature = "rotate_camera")]
+#[derive(Debug)]
+pub struct Vec4f {
+	pub x: f64,
+	pub y: f64,
+	pub z: f64,
+	pub w: f64	
+}
+
+#[cfg(feature = "rotate_camera")]
+impl Vec4f {
+	pub fn new(x: f64, y: f64, z: f64, w: f64) -> Vec4f
+	{
+		Vec4f {
+			x,
+			y,
+			z,
+			w
+		}
+	}
+}
+
+#[cfg(feature = "rotate_camera")]
+pub type Mat4f = [Vec4f; 4];
+
+/// Multiplication of a 4x4 matrix with a 4x1 vector.
+#[cfg(feature = "rotate_camera")]
+
+impl Mul<Vec4f> for Mat4f {
+	type Output = Vec4f;
+	fn mul (self, other: Vec4f) -> Vec4f {
+		let mut res = [0.0; 4];
+		let mut idx = 0;
+		for row in &self {
+			res[idx] += row.x * other.x + row.y * other.y + row.z * other.z + row.w * other.w;
+			idx += 1;
+		}
+		Vec4f::new(res[0], res[1], res[2], res[3])
+	}	
 }
